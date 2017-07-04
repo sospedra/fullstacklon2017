264aecf4517296cd7dad2be7cd993155856189d6
sospedra <sospedra.r@gmail.com>
2017-07-04 23:11:58 +0200

    Add bundler example without test

---
+++ bundler/app.js
@@ -0,0 +1,7 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+
+ReactDOM.render(
+  React.createElement('h1', { children: 'Holis (:' }),
+  document.getElementById('root')
+)
