f3d3974c9d87040b00f887a10422fc312406d419
sospedra <sospedra.r@gmail.com>
2017-07-04 22:37:56 +0200

    Add script feature

---
+++ script/app.spec.js
@@ -0,0 +1,20 @@
+const RESPONSE = 'Mocked React response'
+
+global.React = {
+  createElement: function (tag) {
+    console.log('#1 Tag has to be [ h1 ] --', tag)
+    return RESPONSE
+  }
+}
+
+global.ReactDOM = {
+  render: function (el) {
+    console.log('#2 React element has to be [', RESPONSE, '] --', el)
+  }
+}
+
+global.document = {
+  getElementById: function () {}
+}
+
+require('./app')
